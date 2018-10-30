11d9314cf5176b70ec95659e19a0e11559e69706
priyankalalchandani <priyanka.lalchandani@gmail.com>
2018-10-30 09:33:51 -0500

    first_commit

---
+++ R/hello.R
@@ -0,0 +1,15 @@
+#' @title Predicts Stock Price Movement for Given Stock Symbol
+#'
+#' @description This package predicts whether the stock price at tommorow's market close would be higher or lower compared to today's closing place.
+#'
+#' @param symbol
+#'
+#' @return NULL
+#'
+#' @examples  stock_predict('AAPL')
+#'
+#' @export hello
+#'
+hello <- function() {
+  print("Hello, world!")
+}
