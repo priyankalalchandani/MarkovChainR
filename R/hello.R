501d591cc662763887b1e8297a0ad347ac8fc13c
priyankalalchandani <priyanka.lalchandani@gmail.com>
2018-10-30 15:41:03 -0500

    seventh_commit

--- R/hello.R
+++ R/hello.R
@@ -29,4 +29,36 @@ hello <- function(myname = ""){
   list(
     message = paste(df_res1)
   )
+
+  # extracting a transition matrix
+  df_trans1 <- mod1$transition_matrix
+  df_trans1 <- reshape2 :: dcast(df_trans1, channel_from ~ channel_to, value.var = 'transition_probability')
+
+  ### plotting the Markov graph ###
+  df_trans <- mod1$transition_matrix
+
+  # adding dummies in order to plot the graph
+  df_dummy <- data.frame(channel_from = c('(start)', '(conversion)', '(null)'),
+                         channel_to = c('(start)', '(conversion)', '(null)'),
+                         transition_probability = c(0, 1, 1))
+  df_trans <- dplyr :: rbind(df_trans, df_dummy)
+
+  # ordering channels
+  df_trans$channel_from <- factor(df_trans$channel_from,
+                                  levels = c('(start)', '(conversion)', '(null)', 'c1', 'c2', 'c3'))
+  df_trans$channel_to <- factor(df_trans$channel_to,
+                                levels = c('(start)', '(conversion)', '(null)', 'c1', 'c2', 'c3'))
+  df_trans <- reshape2 :: dcast(df_trans, channel_from ~ channel_to, value.var = 'transition_probability')
+
+  # creating the markovchain object
+  trans_matrix <- matrix(data = as.matrix(df_trans[, -1]),
+                         nrow = nrow(df_trans[, -1]), ncol = ncol(df_trans[, -1]),
+                         dimnames = list(c(as.character(df_trans[, 1])), c(colnames(df_trans[, -1]))))
+  trans_matrix[is.na(trans_matrix)] <- 0
+  trans_matrix1 <- new("markovchain", transitionMatrix = trans_matrix)
+
+  # plotting the graph
+  list(
+    message = paste(trans_matrix1)
+  )
 }
